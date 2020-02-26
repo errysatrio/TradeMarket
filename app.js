50ee8e21a3c88dcccb66429a2f972096fc301da2
Singgit Bramantha <bambadom@gmail.com>
2020-02-26 17:26:30 +0700

    commit1

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ const app = express()
 const port = 3000
 const router = require('./routes')
 
+let user = require('./routes/routerUser.js')
+
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static('public'))
+
+app.use('/user' , user)
 
 app.listen(port, () => {
     console.log('listening to port', port)
