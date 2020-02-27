999d240ded675e40257f1d3d3817807acd1d9bc3
errysatrio <erry.satrio@yahoo.co.id>
2020-02-27 16:17:40 +0700

    routing index

--- controllers/controllerIndex.js
+++ controllers/controllerIndex.js
@@ -2,6 +2,7 @@
 
 const {User, Company, Sequelize} = require('../models')
 const Op = Sequelize.Op
+const bcrypt = require('bcryptjs');
 
 class controllerIndex {
 
@@ -10,40 +11,47 @@ class controllerIndex {
     }
 
     static login(req, res) {
-        const user = req.body.username
-        const password = req.body.password
+        const obj = {
+            password : req.body.password,
+            username : req.body.username
+        }
         const options = {
-            include: [Company],
             where: {
-                [Op.or]: [{ email: user }, { username: user }],
-                [Op.and]: { password: password }
+                [Op.or]: [ { user_name:obj.username }],
+                [Op.and]: { password: obj.password }
             },
             hooks: true
         }
         User
             .findOne(options)
             .then(data => {
-                req.session.user = {
-                    id : data.dataValues.id,
-                    roleName: data.dataValues.Role.name,
-                    isLoggedIn: true
-                }
-                if (data) {
-                    switch (data.dataValues.Role.name) {
-                        case 'client':
-                            res.render(`${data.dataValues.Role.name}`, { data })
-                            break
+                console.log(data)
+                // req.session.user = {
+                //     id : data.dataValues.id,
+                //     roleName: data.dataValues.Role.name,
+                //     isLoggedIn: true
+                // }
+                console.log(req.session.user)
+            //     if (data) {
+            //         switch (data.dataValues.Role.name) {
+            //             case 'client':
+            //                 res.render(``, { data })
+            //                 break
 
-                        case 'admin':
-                            res.redirect(`/${data.dataValues.Role.name}`)
-                            break
-                    }
-                } else {
-                    res.render('login', { data: null, msg: 'Incorrect username / password' })
-                }
+            //             case 'admin':
+            //                 res.redirect(`/${data.dataValues.Role.name}`)
+            //                 break
+            //         }
+            //     } else {
+            //         res.render('login', { data: null, msg: 'Incorrect username / password' })
+            //     }
+            // })
+            // .catch(err => {
+            //     res.render('login', { data: null, msg: 'Incorrect username / password' })
+            res.send(data)
             })
-            .catch(err => {
-                res.render('login', { data: null, msg: 'Incorrect username / password' })
+            .catch(err=>{
+                res.send(err)
             })
     }
 
--- models/user.js
+++ models/user.js
@@ -2,16 +2,24 @@
 module.exports = (sequelize, DataTypes) => {
   const Sequelize = sequelize.Sequelize
   const Model = Sequelize.Model
+  const bcrypt = require('bcryptjs');
   class User extends Model { }
   User.init({
     nama: DataTypes.STRING,
     user_name: DataTypes.STRING,
     password: DataTypes.STRING,
     role: DataTypes.STRING
-  }, { sequelize });
-  User.associate = function (models) {
-    // associations can be defined here
-    User.belongsToMany(models.Company,{through:models.Stock})
-  };
-  return User;
+  }, {
+    hooks: {
+      beforeCreate: (instance,option) =>{
+        let salt = bcrypt.genSaltSync(10);
+        let hash = bcrypt.hashSync(user.password, salt);
+        user.password = hash
+      }
+}, sequelize });
+User.associate = function (models) {
+  // associations can be defined here
+  User.belongsToMany(models.Company, { through: models.Stock })
+};
+return User;
 };
