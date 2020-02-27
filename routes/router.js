514720ffb0347be2ddbaf5c810a829ceac8c300e
errysatrio <erry.satrio@yahoo.co.id>
2020-02-27 17:35:44 +0700

    login done

--- routes/router.js
+++ routes/router.js
@@ -1,5 +1,5 @@
 'use strict'
-
+const session = require('express-session')
 const router = require('express').Router()
 const routerUser = require('./routerUser')
 const routerAdmin = require('./routerAdmin')
@@ -9,7 +9,15 @@ router.get('/',Controller.home)
 router.post('/',Controller.login)
 router.get('/',Controller.register)
 
-router.use('/user',routerUser)
+// router.use('/user',  function (req, res, next) {
+//     console.log('Time:', Date.now())
+//     next()
+//   })
+router.use('/user/:id', function (req, res, next) {
+    console.log('Time:', Date.now())
+    next()
+  }, routerUser)
+
 router.use('/admin',routerAdmin)
 
 module.exports = router
--- routes/routerUser.js
+++ routes/routerUser.js
@@ -5,7 +5,10 @@ const ControllerUser = require('../controllers/controllerUser')
 
 router.get('/',ControllerUser.home)
 router.get('/companyLists',ControllerUser.buy)
-router.get('/add',ControllerUser.addForm)
+router.get('/add', function (req, res, next) {
+    console.log('Time3:', Date.now())
+    next()
+  }, ControllerUser.addForm)
 router.post('/add',ControllerUser.addData)
 
 module.exports = router
